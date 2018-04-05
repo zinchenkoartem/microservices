32afe2a3b40f1f3b6f36ba18af66cb03edd4f95f
Artem Z <artem_z@ukr.net>
2018-04-05 11:44:58 +0300

    setup git to config service

--- device-service/src/main/java/com/azinchenko/deviceservice/controller/DeviceController.java
+++ device-service/src/main/java/com/azinchenko/deviceservice/controller/DeviceController.java
@@ -5,6 +5,8 @@ import com.azinchenko.deviceservice.entity.Device;
 import com.azinchenko.deviceservice.entity.FullDevice;
 import com.azinchenko.deviceservice.repository.DeviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +18,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@RefreshScope
 @RequestMapping("/device-service")
 public class DeviceController {
 
@@ -25,15 +28,19 @@ public class DeviceController {
   @Autowired
   private PriceServiceClient priceServiceClient;
 
+  @Value("${custom.device.price.multiplier}")
+  private Double multiplier;
+
   @RequestMapping("/all")
   public List<FullDevice> getAll() {
-    System.out.println("++++++++device-servise is called!!!!!!!!!");
+    System.out.println("++++++++device-service is called!!!!!!!!!");
+    System.out.println("multiplier is: " + multiplier);
     Map<String, Double> prices = priceServiceClient.getPrice();
     List<Device> devices = repository.findAll();
     return devices.stream().map(device -> {
       String name = device.getName();
       prices.get(name);
-      return new FullDevice(name, prices.get(name));
+      return new FullDevice(name, prices.get(name) * multiplier);
     }).collect(Collectors.toList());
   }
 
