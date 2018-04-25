b5b2e3c6d0387a5681542a00561497d9a4341d8d
Artem Z <artem_z@ukr.net>
2018-04-25 17:17:29 +0300

    change

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf proj/
-docker stop "$(docker ps -a -q)"
-docker rm -f "$(docker ps -a -q)"
-docker rmi "$(docker images -q)"
+docker stop $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
+docker rmi $(docker images -q)
