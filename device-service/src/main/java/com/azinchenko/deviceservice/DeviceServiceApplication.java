166822471f26a34e9a8b0236b5c748a86de2eab3
Artem Z <artem_z@ukr.net>
2018-04-26 15:40:40 +0300

    change

--- device-service/src/main/java/com/azinchenko/deviceservice/DeviceServiceApplication.java
+++ device-service/src/main/java/com/azinchenko/deviceservice/DeviceServiceApplication.java
@@ -2,12 +2,14 @@ package com.azinchenko.deviceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class DeviceServiceApplication {
 
 	public static void main(String[] args) {
--- price-service/src/main/java/com/azinchenko/monitor/PriceServiceApplication.java
+++ price-service/src/main/java/com/azinchenko/monitor/PriceServiceApplication.java
@@ -2,10 +2,12 @@ package com.azinchenko.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class PriceServiceApplication {
 
 	public static void main(String[] args) {
