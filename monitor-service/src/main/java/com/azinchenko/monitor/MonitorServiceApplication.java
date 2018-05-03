ed9e7eb46ed9c704ada779de7f19bc8b59f15fca
Artem Z <artem_z@ukr.net>
2018-05-03 17:26:30 +0300

    change

--- monitor-service/src/main/java/com/azinchenko/monitor/MonitorServiceApplication.java
+++ monitor-service/src/main/java/com/azinchenko/monitor/MonitorServiceApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.stream.converter.CompositeMessageConverterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.support.converter.ConfigurableCompositeMessageConverter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,4 +18,10 @@ public class MonitorServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorServiceApplication.class, args);
 	}
+
+	@Bean
+	public ConfigurableCompositeMessageConverter integrationArgumentResolverMessageConverter(CompositeMessageConverterFactory factory) {
+		return new ConfigurableCompositeMessageConverter(factory.getMessageConverterForAllRegistered().getConverters());
+	}
+
 }
